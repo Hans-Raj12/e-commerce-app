ff5b421a78284d4b011a0305a0550901282a4d3b
Hans Raj <75563900+Hans-Raj12@users.noreply.github.com>
2023-08-20 14:33:01 +0500

    Add layout main content with navbar and footer, also add pages using react-router

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
+
+function NavBar() {
+  return (
+    <div>
+      <div className='navbar'>
+        <div className="wrapper">
+            <div className='left'></div>
+            <div className='center'></div>
+            <div className='right'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
