ea360bd270afff4561087a7533148182844a04ff
Hans Raj <75563900+Hans-Raj12@users.noreply.github.com>
2023-08-27 09:57:49 +0500

    add code to fetch single product from the CMS (strapi)

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -5,30 +5,34 @@ import './Product.scss';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BalanceIcon from '@mui/icons-material/Balance';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+
 const Product = () => {  
     
+    const id = useParams().id;
     const [quantity, setQuantity] = useState(1)
-    const [seletedIMage, setSelectedImage] = useState(0)
-    const images = [
-        'https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-        'https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'
-    ]
+    const [selectedImage, setSelectedImage] = useState('img')
     
+    const {data, loading, error} = useFetch(`/products/${id}?populate=*`)
+
     return (
         <div className='product'>
+            {loading ? 'loading...' : (
+            <>
             <div className="left">
                 <div className="images">
-                    <img src={images[0]} alt="" onClick={(e)=>setSelectedImage(0)}/>
-                    <img src={images[1]} alt="" onClick={(e)=>setSelectedImage(1)}/>
+                    <img src={process.env.REACT_APP_UPLOAD_URL+data?.attributes?.img?.data?.attributes?.url} alt="" onClick={(e)=>setSelectedImage('img')}/>
+                    <img src={process.env.REACT_APP_UPLOAD_URL+data?.attributes?.img2?.data?.attributes?.url} alt="" onClick={(e)=>setSelectedImage('img2')}/>
                 </div>
                 <div className="mainImage">
-                    <img src={images[seletedIMage]} alt="" />
+                    <img src={process.env.REACT_APP_UPLOAD_URL+data?.attributes[selectedImage]?.data?.attributes?.url} alt="" />
                 </div>
             </div>
             <div className="right">
-                <h1>Title</h1>
-                <span className='price'>$199</span>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio doloribus nulla recusandae voluptate explicabo. Aspernatur ipsam voluptatem at nihil, eveniet obcaecati rerum esse distinctio, amet dolorum illum quam. Vitae, quidem.</p>
+                <h1>{data?.attributes?.title}</h1>
+                <span className='price'>${data?.attributes?.price}</span>
+                <p>{data?.attributes?.desc}</p>
 
               <div className="quantity">
               <button onClick={()=>setQuantity(prev=>prev===1?1:prev-1)}>-</button>
@@ -63,6 +67,8 @@ const Product = () => {
                     <span>FAQ</span>
                 </div>
             </div>
+            </>)
+            }
         </div>
     )
 }
